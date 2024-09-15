67bf9be695adb9acae28aac53ec16c4c5770e8a6
Meany <mirmuzeen34@gmail.com>
2024-09-15 16:09:43 -0500

    Add files via upload

---
+++ Muzeen_Merge.py
@@ -0,0 +1,61 @@
+import random
+from timeit import default_timer as timer
+
+a = int(input('enter the list size: '))
+b = int(input('enter the starting number: '))
+c = int(input('enter the ending number: '))
+
+def mergeSort(l, s):
+    listx = []
+    y = 0
+
+    if a%2 != 0:
+        listx.append([l[y],l[y+1],l[y+2]])
+        y = 3    
+        for i in range(s-1):
+            listx.append([l[y], l[y+1]])
+            y = y+2
+            if s == y:
+                break
+        #print('broken down list: ', listx)
+        
+    else:
+        m = 0
+        for i in range(s-1):
+            listx.append([l[m], l[m+1]])
+            m = m+2
+            if s == m:
+                break
+#        print('broken down list: ', listx)
+    l = 0
+    while len(listx) > 1:
+            final_list = []        
+            for i in range(0, len(listx)-1, 2):
+
+                final_list.append(listx[i] + listx[i+1])
+                final_list[-1].sort()
+                global z
+                z +=1
+            if len(final_list) == 1 and l == 0:
+                final_list.append(listx[-1])
+                final_list.sort()
+                l +=1
+            listx = final_list
+  #  print(listx)
+
+z = 0
+list = []
+for i in range(a):
+    x = random.randint(b,c)
+    list.append(x)
+#print('unsorted list: ', list)
+o = len(list)
+
+if b < c:
+    start = timer()
+    mergeSort(list, a)
+    end = timer()
+    print('time elapsed is: ', end - start)
+    print(z)
+else:
+    print('some error occured!')
---
+++ insertion.py
@@ -0,0 +1,32 @@
+from timeit import default_timer as timer
+import random
+ns = int(input('enter number of elements: '))
+ls = int(input('enter start point: '))
+le = int(input('enter end point: '))
+a = str(ns)
+len(a)
+start = timer()
+list = []
+for m in range(ns):
+    n = random.randint(ls,le)
+    list.append(n)
+a = len(list)
+print(a)
+#print('unsortted: ', list)
+temp = 0
+newlist = []
+while newlist != list:
+    i = 0
+    j = 1
+    newlist = list[:]
+    while i < a-1:
+        if list[i]>list[j]:
+            temp = list[i]
+            list[i]= list[j]
+            list[j] = temp
+        i = i+1
+        j = j+1
+end = timer()
+print(end - start)
+#print('sorted: ', list)
+print('number of elements produced: ', a)
